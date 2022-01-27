dfc359d5507150320a1293e4df531c256e7d36dc
Fred <fredlefevre@outlook.com>
2022-01-27 16:51:26 -0500

    Modified build and deploy scripts and integrated morgan library for logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -9,7 +11,13 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
