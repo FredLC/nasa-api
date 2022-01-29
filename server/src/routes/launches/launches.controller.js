e98d0ddce1ff3c52c3da8a41df8a98d6eadc1e10
Fred <fredlefevre@outlook.com>
2022-01-28 19:05:14 -0500

    Implemented DELETE /launches endpoint

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 const httpGetAllLaunches = (req, res) => {
   return res.status(200).json(getAllLaunches());
@@ -29,7 +34,21 @@ const httpAddNewLaunch = (req, res) => {
   return res.status(201).json(launch);
 };
 
+const httpAbortLaunch = (req, res) => {
+  const launchId = Number(req.params.id);
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "Launch does not exist",
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+};
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
